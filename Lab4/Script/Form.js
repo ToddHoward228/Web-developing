f3fc18a21bdb821ff6d89a0ab96fff1cda16b8cb
ToddHoward228 <164908010+ToddHoward228@users.noreply.github.com>
2024-09-10 14:16:49 +0300

    Add files via upload

---
+++ Lab4/Script/Form.js
@@ -0,0 +1,21 @@
+﻿
+let join_menu = document.getElementById("join_menu")
+let sigin_form = document.getElementById("signin");
+let login_form = document.getElementById("login");
+
+function Login() {
+    join_menu.style.display = "none";
+    login_form.style.display = "block";
+}
+
+function Signin() {
+    join_menu.style.display = "none";
+    sigin_form.style.display = "block";
+
+}
+
+function JoinMenu() {
+    login_form.style.display = "none";
+    sigin_form.style.display = "none";
+    join_menu.style.display = "block";
+}
